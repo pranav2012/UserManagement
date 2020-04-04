923e350c5b1976f00bac40341d78bae5d6a4e4ed
Pranav Agarwal <p2012agarwal@gmail.com>
2020-04-04 21:22:08 +0530

    Updates

---
+++ precache-manifest.9f85066d0b7b3fae5b2b08a3b63d0f5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c89074a005f5027a749701aa6d20963",
+    "url": "/pranav2012/UserManagement/index.html"
+  },
+  {
+    "revision": "eee0aa5c4d6c2dc37ffd",
+    "url": "/pranav2012/UserManagement/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "0ee0cb886097f5e6043b",
+    "url": "/pranav2012/UserManagement/static/css/main.66232929.chunk.css"
+  },
+  {
+    "revision": "eee0aa5c4d6c2dc37ffd",
+    "url": "/pranav2012/UserManagement/static/js/2.19ca09a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pranav2012/UserManagement/static/js/2.19ca09a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee0cb886097f5e6043b",
+    "url": "/pranav2012/UserManagement/static/js/main.a68b184e.chunk.js"
+  },
+  {
+    "revision": "aee4976fbcfe7316f422",
+    "url": "/pranav2012/UserManagement/static/js/runtime-main.f9d4c10e.js"
+  }
+]);
